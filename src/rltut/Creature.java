2abe895dd1747068a24be12d55e544e6247d4b09
Jeremy Rist <rist.jeremy@gmail.com>
2012-07-26 15:29:01 -0400

    Added a creature class

---
+++ src/rltut/Creature.java
@@ -0,0 +1,35 @@
+package rltut;
+
+import java.awt.Color;
+
+public class Creature {
+	private World world;
+
+	public int x;
+	public int y;
+
+	private char glyph;
+
+	private Color color;
+
+	private CreatureAi ai;
+
+	public Creature(World world, char glyph, Color color) {
+		this.world = world;
+		this.glyph = glyph;
+		this.color = color;
+	}
+
+	public Color color() {
+		return color;
+	}
+
+	public char glyph() {
+		return glyph;
+	}
+
+	public void setCreatureAi(CreatureAi ai) {
+		this.ai = ai;
+	}
+
+}
