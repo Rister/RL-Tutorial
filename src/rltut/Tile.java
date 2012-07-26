9796bf0451b3162fe86c2fef1223fd8122f4a70e
Jeremy Rist <rist.jeremy@gmail.com>
2012-07-26 15:10:03 -0400

    cleaned up the formatting.

--- src/rltut/Tile.java
+++ src/rltut/Tile.java
@@ -5,18 +5,23 @@ import java.awt.Color;
 import asciiPanel.AsciiPanel;
 
 public enum Tile {
-    FLOOR((char)250, AsciiPanel.yellow),
-    WALL((char)177, AsciiPanel.yellow),
-    BOUNDS('x', AsciiPanel.brightBlack);
+	FLOOR((char) 250, AsciiPanel.yellow), WALL((char) 177, AsciiPanel.yellow), BOUNDS(
+			'x', AsciiPanel.brightBlack);
 
-    private char glyph;
-    public char glyph() { return glyph; }
+	private char glyph;
 
-    private Color color;
-    public Color color() { return color; }
+	public char glyph() {
+		return glyph;
+	}
 
-    Tile(char glyph, Color color) {
-	this.glyph = glyph;
-	this.color = color;
-    }
+	private Color color;
+
+	public Color color() {
+		return color;
+	}
+
+	Tile(char glyph, Color color) {
+		this.glyph = glyph;
+		this.color = color;
+	}
 }
--- src/rltut/world.java
+++ src/rltut/world.java
@@ -3,30 +3,37 @@ package rltut;
 import java.awt.Color;
 
 public class World {
-    private Tile[][] tiles;
-    private int width;
-    public int width() { return width; }
+	private Tile[][] tiles;
+	private int width;
 
-    private int height;
-    public int height() { return height; }
+	public int width() {
+		return width;
+	}
 
-    public World(Tile[][] tiles) {
-	this.tiles = tiles;
-	this.width = tiles.length;
-	this.height = tiles[0].length;
-    }
+	private int height;
 
-    public Tile tile(int x, int y) {
-	if (x < 0 || x >= width || y < 0 || y >= height)
-	    return Tile.BOUNDS;
-	else
-	    return tiles[x][y];
-    }
+	public int height() {
+		return height;
+	}
 
-    public char glyph(int x, int y) {
-	return tile(x, y).glyph();
-    }
-    public Color color(int x, int y) {
-	return tile(x, y).color();
-    }
+	public World(Tile[][] tiles) {
+		this.tiles = tiles;
+		this.width = tiles.length;
+		this.height = tiles[0].length;
+	}
+
+	public Tile tile(int x, int y) {
+		if (x < 0 || x >= width || y < 0 || y >= height)
+			return Tile.BOUNDS;
+		else
+			return tiles[x][y];
+	}
+
+	public char glyph(int x, int y) {
+		return tile(x, y).glyph();
+	}
+
+	public Color color(int x, int y) {
+		return tile(x, y).color();
+	}
 }
