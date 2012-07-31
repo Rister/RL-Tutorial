eff981f3f05b762c290414d5d546f137efffba16
Jeremy Rist <rist.jeremy@gmail.com>
2012-07-31 16:27:06 -0400

    Added stairs to the tile enum

--- src/rltut/Tile.java
+++ src/rltut/Tile.java
@@ -5,8 +5,11 @@ import java.awt.Color;
 import asciiPanel.AsciiPanel;
 
 public enum Tile {
-	FLOOR((char) 250, AsciiPanel.yellow), WALL((char) 177, AsciiPanel.yellow), BOUNDS(
-			'x', AsciiPanel.brightBlack);
+	FLOOR((char) 250, AsciiPanel.yellow),
+	WALL((char) 177, AsciiPanel.yellow),
+	BOUNDS('x', AsciiPanel.brightBlack),
+	STAIRS_DOWN('>', AsciiPanel.white),
+	STAIRS_UP('<', AsciiPanel.white);
 
 	private char glyph;
 
