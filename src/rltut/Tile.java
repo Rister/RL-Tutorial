4dfbdc5fc95bd3d3d53f053eb828109c5a94709b
Jeremy Rist <rist.jeremy@gmail.com>
2012-08-02 11:57:14 -0400

    Added a blank tile type

--- src/rltut/Tile.java
+++ src/rltut/Tile.java
@@ -7,7 +7,7 @@ import asciiPanel.AsciiPanel;
 public enum Tile {
 	FLOOR((char) 250, AsciiPanel.yellow), WALL((char) 177, AsciiPanel.yellow), BOUNDS(
 			'x', AsciiPanel.brightBlack), STAIRS_DOWN('>', AsciiPanel.white), STAIRS_UP(
-			'<', AsciiPanel.white);
+			'<', AsciiPanel.white), UNKNOWN(' ', AsciiPanel.white);
 
 	private char glyph;
 
